44841cf015e656eeefdb75643a26f862744aa48b
Sultan Suleimenov <ss.softwareit@gmail.com>
2022-10-26 20:39:10 +0600

    added profile page to bottom navigation

--- app/src/main/java/com/example/myapplication/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/profile/ProfileFragment.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication.ui.search
+package com.example.myapplication.ui.profile
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,14 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.myapplication.R
 
-class SearchFragment : Fragment() {
+class ProfileFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
 }
